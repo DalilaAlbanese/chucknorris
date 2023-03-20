0b3c5eed28357e0e5f22d5e1c434031c5a2b8978
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2023-03-20 19:21:40 +0100

    funzione handle

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,8 +2,11 @@ import React from "react" //chiammiamo react
 import { useState } from 'react'
 import '../styles/Dropdown.css'
 
-const Dropdown =()=>{
+const Dropdown =({handle})=>{
     const [categories, setCategories] = useState([])
+    function handleChange(e){
+      handle(e.target.value)
+    }
     function  getCategories(){
         fetch('https://api.chucknorris.io/jokes/categories').then(
           response => response.json()
@@ -14,7 +17,7 @@ const Dropdown =()=>{
         )
       }
     return (
-        <select name="menu" className='menu' onClick={()=>getCategories()}>   
+        <select name="menu" className='menu' onClick={()=>getCategories()} onChange={handleChange(e)}>   
         {categories.map((category, index)=>(
             <option value={category} key={index}>{category}</option>
         ))}
