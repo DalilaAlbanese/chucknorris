a8cb30849d888b9392cacdff3fe3fd97d92c8617
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2023-03-22 19:29:54 +0100

    finito logica
    fare lo stile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css'
 import Button from './components/Button.jsx'
 import Dropdown from './components/Dropdown.jsx'
 import JokeRender from './components/JokeRender'
+import logoChuck from '../public/logo.svg'
 function App() {
   const [count, setCount] = useState(0)
   const [joke, setJoke] = useState("")
@@ -10,15 +11,34 @@ function App() {
   const [selection, setSelection]= useState("")
 
   function  loadJoke(){
-    fetch('').then(
+    if(selection != ""){
+    let url = `https://api.chucknorris.io/jokes/random?category=${selection}`
+    let promise = fetch(url)
+
+    promise.then(
       response => response.json()
     ).then(
-      data=> {
-        setJoke(data.value)
-      }
-    )
-    
+      data => setJoke(data.value)
+    ) 
+  }else{
+    let url = `https://api.chucknorris.io/jokes/random`
+    let promise = fetch(url)
+
+    promise.then(
+      response => response.json()
+    ).then(
+      data => setJoke(data.value)
+    ) 
+
   }
+}
+
+function copy(){
+  if(joke != ""){
+    navigator.clipboard.writeText(joke)
+    alert("Il testo è stato copiato")
+  }
+}
 
   function handleChange(e){
       setSelection(e)
@@ -26,13 +46,16 @@ function App() {
 
   return (
     <div className="App">
+        <h1>Webapp API Chuck Norris</h1>
+        <p>Design di una pagina che utilizza la API di chucknorris.io per generare alla pressione di un pulsante una battuta del tipo che selezioni nel menu a tendina qui sotto.</p>
+        <img src={logoChuck}/>
         <Dropdown handle={handleChange}/>
         {joke != "" &&
         <JokeRender joke={joke} />
 
         }
-        <Button  type='primary' text='CARICA JOKE'/> 
-        <Button type='secondary' text='COPIA TESTO'/> 
+        <Button  type='primary' text='CARICA JOKE' action={()=>loadJoke()}/> 
+        <Button type='secondary' text='COPIA TESTO' action={copy}/> 
 
     </div>
   )
